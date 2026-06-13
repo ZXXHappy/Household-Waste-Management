17840ef092eea0e7964ca7bd07384dfd9b0ae39b
ZXXHappy <2762943477@qq.com>
2026-06-14 00:59:05 +0800

    补main.js

--- vue/src/main.js
+++ vue/src/main.js
@@ -17,3 +17,4 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
+//补main.js
